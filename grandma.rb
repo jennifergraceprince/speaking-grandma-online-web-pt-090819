1581707b4ef69f13cb71ba97e166d439c19b83e5
Jennifer Prince <jennifergraceprince@gmail.com>
2019-09-24 05:05:48 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,7 @@
+
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
-    "I LOVE YOU TOO PUMPKIN!"
+    return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
   else phrase
